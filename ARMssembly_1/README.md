b6c72db0eaa8b7830e01a52334f0f1bd58e643aa
pde-bakk <pde-bakk@student.codam.nl>
2023-01-12 14:37:18 +0100

    arm

--- ARMssembly_1/README.md
+++ ARMssembly_1/README.md
@@ -1,1 +1,8 @@
 # ARMssembly_1
+
+I added comments for clarification in the assembly file.
+
+The answer is 90, convert that to 32 bits hex:
+```shell
+python3 -c 'print(f"picoCTF{{{90:08x}}}")'
+```
--- ARMssembly_1/chall_1.s
+++ ARMssembly_1/chall_1.s
@@ -6,25 +6,25 @@
 	.type	func, %function
 func:
 	sub	sp, sp, #32
-	str	w0, [sp, 12]
+	str	w0, [sp, 12] // store user_input at stack+12
 	mov	w0, 68
-	str	w0, [sp, 16]
+	str	w0, [sp, 16] // store 68 at stack+16
 	mov	w0, 2
-	str	w0, [sp, 20]
+	str	w0, [sp, 20] // store 2 at stack+20
 	mov	w0, 3
-	str	w0, [sp, 24]
-	ldr	w0, [sp, 20]
-	ldr	w1, [sp, 16]
-	lsl	w0, w1, w0
-	str	w0, [sp, 28]
-	ldr	w1, [sp, 28]
-	ldr	w0, [sp, 24]
-	sdiv	w0, w1, w0
-	str	w0, [sp, 28]
-	ldr	w1, [sp, 28]
-	ldr	w0, [sp, 12]
-	sub	w0, w1, w0
-	str	w0, [sp, 28]
+	str	w0, [sp, 24] // // store 3 at stack+24
+	ldr	w0, [sp, 20] // var w0 = 2
+	ldr	w1, [sp, 16] // var w1 = 68
+	lsl	w0, w1, w0	// logical left shift: w0 = w1 << w0 = 68 << 2 = 272
+	str	w0, [sp, 28] // store 272 at stack=28
+	ldr	w1, [sp, 28] // w1 = 272
+	ldr	w0, [sp, 24] // w1 = 3
+	sdiv	w0, w1, w0 // signed_divide: w0 = w1 / w0 = 90
+	str	w0, [sp, 28] // store 90 at stack+28
+	ldr	w1, [sp, 28] // w1 = 90
+	ldr	w0, [sp, 12] // w0 = user_input
+	sub	w0, w1, w0 // w0 = 90 - user_input
+	str	w0, [sp, 28] // store (90 - user_input) at stack+28
 	ldr	w0, [sp, 28]
 	add	sp, sp, 32
 	ret
@@ -43,22 +43,22 @@ func:
 main:
 	stp	x29, x30, [sp, -48]!
 	add	x29, sp, 0
-	str	w0, [x29, 28] ; 68
-	str	x1, [x29, 16] ; 2
-	ldr	x0, [x29, 16] ; 3
+	str	w0, [x29, 28]
+	str	x1, [x29, 16]
+	ldr	x0, [x29, 16]
 	add	x0, x0, 8
 	ldr	x0, [x0]
-	bl	atoi
-	str	w0, [x29, 44]
+	bl	atoi // call atoi on user input
+	str	w0, [x29, 44] // store (int)argv1 at x29+44
 	ldr	w0, [x29, 44]
 	bl	func
 	cmp	w0, 0
-	bne	.L4
+	bne	.L4 // if (func(w0) != 0) goto .LC4
 	adrp	x0, .LC0
 	add	x0, x0, :lo12:.LC0
 	bl	puts
 	b	.L6
-.L4:
+.L4: // .L4 will call .LC1 which makes us lose :(
 	adrp	x0, .LC1
 	add	x0, x0, :lo12:.LC1
 	bl	puts
